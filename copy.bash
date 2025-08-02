4452b5a9281a055c7f183136b9b1f222d08590bf
Rui Luo <luorui2023@shanghaitech.edu.cn>
2025-08-02 21:20:33 +0800

    track copy.bash

---
+++ copy.bash
@@ -0,0 +1,10 @@
+cp -r ../MrAutoGrad_Private/mrautograd_src ./
+cp -r ../MrAutoGrad_Private/example ./
+cp ../MrAutoGrad_Private/benchmark.py ./
+cp ../MrAutoGrad_Private/LICENSE ./
+cp ../MrAutoGrad_Private/MANIFEST.in ./
+cp ../MrAutoGrad_Private/pyproject.toml ./
+cp ../MrAutoGrad_Private/README.md ./
+cp ../MrAutoGrad_Private/setup.py ./
+
+rm -r ./mrautograd_src/ext/mtg
